2c121c7e1d1467e957d6dac8d9f6a45a09aad8e1
Alfonso <78657787+EdEngineering@users.noreply.github.com>
2024-11-16 19:49:51 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/salgamos-Dani-<3",
+  base: "https://EdEngineering.github.io/quieres-salir-conmigo",
   plugins: [react()],
 });
